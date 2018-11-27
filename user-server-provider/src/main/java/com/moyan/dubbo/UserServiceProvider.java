87bc307ce9312adf4222bdc329860d88819699d9
moyan595 <T123h11L96>
2018-11-28 00:24:14 +0800

    user-service-provider

---
+++ user-server-provider/src/main/java/com/moyan/dubbo/UserServiceProvider.java
@@ -0,0 +1,20 @@
+package com.moyan.dubbo;
+
+import java.io.IOException;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 提供方
+ * @author 莫言
+ *
+ */
+public class UserServiceProvider {
+	
+	public static void main(String [] args) throws IOException {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("provider.xml");
+		context.start();
+		System.in.read();
+	}
+
+}

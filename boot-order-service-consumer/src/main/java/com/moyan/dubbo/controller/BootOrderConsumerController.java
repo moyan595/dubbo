f3c009ac86c5320793971fb5357384f17a0dcb1d
moyan595 <T123h11L96>
2018-11-30 09:46:55 +0800

    66666

--- boot-order-service-consumer/src/main/java/com/moyan/dubbo/controller/BootOrderConsumerController.java
+++ boot-order-service-consumer/src/main/java/com/moyan/dubbo/controller/BootOrderConsumerController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.moyan.dubbo.bean.UserAddress;
 import com.moyan.dubbo.service.OrderService;
 
-
+//
 
 @RequestMapping("/order")
 @RestController

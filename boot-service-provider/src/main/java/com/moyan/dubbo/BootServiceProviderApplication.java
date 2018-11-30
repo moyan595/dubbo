a8dfe18c8a189b31f34154abba3636c563a34c9e
moyan595 <T123h11L96>
2018-11-30 09:46:28 +0800

    33

--- boot-service-provider/src/main/java/com/moyan/dubbo/BootServiceProviderApplication.java
+++ boot-service-provider/src/main/java/com/moyan/dubbo/BootServiceProviderApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 
+//
 
 @EnableDubbo //开启dubbo注解支持
 @SpringBootApplication
